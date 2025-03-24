2f75a0ff3fcd7ca77f63e924e1775dc4767c021f
James Emerson (An Vo) <vo.an.2503@gmail.com>
2025-03-23 23:11:05 -0700

    Update ChartRAG.js

--- frontend/components/ChartRAG.js
+++ frontend/components/ChartRAG.js
@@ -3,9 +3,9 @@ import UploadSection from './UploadSection';
 
 const ChartRAG = ({ summary, setSummary }) => {
   return (
-<div className="min-h-[75vh] min-w-[75vw] bg-white shadow-md rounded-lg p-6 mb-4 flex flex-col justify-center items-center">
+<div className="min-h-[75vh] min-w-[75vw] bg-white/80 shadow-md rounded-lg p-6 mb-4 flex flex-col justify-center items-center">
       <h1 className="text-5xl font-bold mb-4 text-center">ChartRAG</h1>
-      <p className="text-lg mb-8 text-center">
+<p className="text-lg mb-8 text-center bg-white px-4 py-2">
         Upload a csv file and our ai will summarise for you
       </p>
       <div className="flex flex-col items-center">
